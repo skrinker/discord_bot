b43bd882c6441b53fb899dad8a7883171ca75aa3
skrinker <guzel28sit@gmail.com>
2020-11-10 05:22:44 +0300

    make it better

--- commands.js
+++ commands.js
@@ -8,8 +8,9 @@ const commands = {
         return "Error, maximum value should be more than minimum.";
       if (args.length == 0) return "Error, input a number.";
       return (
-        Math.floor(Math.random() * ((args[1] || 0) - args[0])) +
-        parseInt(args[0])
+        Math.floor(
+          Math.random() * (parseInt(args[1] || 0) - parseInt(args[0]))
+        ) + parseInt(args[0])
       );
     },
   },
