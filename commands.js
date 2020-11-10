57747acd00c4ff97491f0dab2eb501f0a4570bd5
skrinker <guzel28sit@gmail.com>
2020-11-10 05:24:03 +0300

    make it better

--- commands.js
+++ commands.js
@@ -4,13 +4,12 @@ const commands = {
     decription:
       "Generate random number between 0 and input number or between two numbers.",
     process: (args) => {
-      if (args[0] > args[1])
+      if (parseInt(args[0]) > parseInt(args[1]))
         return "Error, maximum value should be more than minimum.";
       if (args.length == 0) return "Error, input a number.";
       return (
-        Math.floor(
-          Math.random() * (parseInt(args[1] || 0) - parseInt(args[0]))
-        ) + parseInt(args[0])
+        Math.floor(Math.random() * ((args[1] || 0) - args[0])) +
+        parseInt(args[0])
       );
     },
   },
